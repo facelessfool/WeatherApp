45b05fc1acfafaea347665f823b00acccc0821b2
Kajal Chingsubam <kchings1@binghamton.edu>
2020-07-19 18:21:48 -0400

    added search city function

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
 console.log("connected");
-var cityName;
+var cityName = "New York";
 var temp1;
+var cityElement = document.getElementById("city-box");
 var weatherElement = document.getElementById("weather-box");
 var locationElement = document.getElementById("location-box");
-api =
-  "http://api.openweathermap.org/data/2.5/weather?id=1273294&appid=a3620f6650ae7d3b721fd916be45e214";
+const apiKey = "a3620f6650ae7d3b721fd916be45e214";
 
-fetch(api)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    temp1 = data.main.temp;
-    cityName = data.name;
+cityElement.addEventListener("keypress", check);
+function check(event) {
+  if (event.keyCode == 13) {
+    console.log(cityElement.value);
+    cityName = cityElement.value;
+    findTemp(cityName);
+  }
+}
 
-    console.log(temp1);
-  })
-  .then(function () {
-    displayWeather();
-  });
+function findTemp(cityName) {
+  api = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a3620f6650ae7d3b721fd916be45e214`;
+
+  fetch(api)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      temp1 = data.main.temp;
+      cityName = data.name;
+
+      console.log(temp1);
+    })
+    .then(function () {
+      displayWeather();
+    });
+}
 
 function displayWeather() {
-  weatherElement.innerHTML = Math.floor(temp1 - 273);
-  locationElement.innerHTML = cityName;
+  // <div id="weather-box">-- <span>°</span></div>
+  weatherElement.innerText = Math.floor(temp1 - 273) + "°C";
+  locationElement.innerText = cityName;
+}
